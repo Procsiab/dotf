f1ec8d4a6893b46302f76b4c57c5ffbeb671ee4a
Lorenzo Prosseda <lerokamut@gmail.com>
2019-09-07 10:48:01 +0200

    GTK notification on night mode toggle

--- nightmode/.local/bin/toggle-nm.sh
+++ nightmode/.local/bin/toggle-nm.sh
@@ -2,6 +2,8 @@
 
 if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" == "'Adwaita'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+    notify-send "Night mode is ENABLED"
 else
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
+    notify-send "Night mode is DISABLED"
 fi
