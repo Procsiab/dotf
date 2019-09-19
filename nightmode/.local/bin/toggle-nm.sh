2624b1905839866180fc27e364ab5b91138f4609
Lorenzo Prosseda <lerokamut@gmail.com>
2019-09-19 10:23:14 +0200

    Added light color scheme to Tilix terminal

--- nightmode/.local/bin/toggle-nm.sh
+++ nightmode/.local/bin/toggle-nm.sh
@@ -2,8 +2,10 @@
 
 if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" == "'Adwaita'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-    notify-send "Night mode is ENABLED"
+    gsettings set com.gexperts.Tilix.Profile:/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/ palette "['#282828', '#CC241D', '#98971A', '#D79921', '#458588', '#B16286', '#689D6A', '#A89984', '#FBF1C7', '#FB4934', '#B8BB26', '#FABD2F', '#84A598', '#D3869B', '#8EC07C', '#EBDBB2']"
+    notify-send "Night Mode" "Dark theme is enabled"
 else
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
-    notify-send "Night mode is DISABLED"
+    gsettings set com.gexperts.Tilix.Profile:/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/ palette "['#F9F5D7', '#CC241D', '#98971A', '#D79921', '#458588', '#B16286', '#689D6A', '#7C6F64', '#928374', '#9D0006', '#79740E', '#B57614', '#076678', '#8F3F71', '#427B58', '#3C3836']"
+    notify-send "Night mode" "Light theme is enabled"
 fi
