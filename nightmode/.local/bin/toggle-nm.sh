8f13fd5000c28a6978138e8da4c3a714cc1de62d
Lorenzo Prosseda <lerokamut@gmail.com>
2022-05-10 22:55:50 +0200

    Updated night mode toggle for GNOME 42

--- nightmode/.local/bin/toggle-nm.sh
+++ nightmode/.local/bin/toggle-nm.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" == "'Adwaita'" ]; then
-    # Toggle DARK theme
+if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" == "'default'" ]; then
+    # Toggle DARK variant
+    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+    # Legacy app dark theme
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
     # Change Tilix color palette
     gsettings set com.gexperts.Tilix.Profile:/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/ palette "['#282828', '#CC241D', '#98971A', '#D79921', '#458588', '#B16286', '#689D6A', '#A89984', '#FBF1C7', '#FB4934', '#B8BB26', '#FABD2F', '#84A598', '#D3869B', '#8EC07C', '#EBDBB2']"
@@ -17,7 +19,9 @@ if [ "$(gsettings get org.gnome.desktop.interface gtk-theme)" == "'Adwaita'" ]; 
     # Notify the user
     notify-send "Night Mode" "Dark theme is enabled" -i weather-clear-night -t 3000
 else
-    # Toggle DARK theme
+    # Toggle LIGHT variant
+    gsettings set org.gnome.desktop.interface color-scheme "default"
+    # Legacy app light theme
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
     # Change Tilix color palette
     gsettings set com.gexperts.Tilix.Profile:/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/ palette "['#F9F5D7', '#CC241D', '#98971A', '#D79921', '#458588', '#B16286', '#689D6A', '#7C6F64', '#928374', '#9D0006', '#79740E', '#B57614', '#076678', '#8F3F71', '#427B58', '#3C3836']"
